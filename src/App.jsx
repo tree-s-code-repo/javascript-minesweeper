66fcf699b2225fff4f89a32bb5fd389c1cc8ea8f
tree <hhyjs2r@gmail.com>
2024-09-22 23:18:14 +0900

    test: place holder and initialdata:

--- src/App.jsx
+++ src/App.jsx
@@ -10,55 +10,57 @@ export const getTodo = async (id) => {
   return res.data;
 };
 
-export const getTodo222 = async (id) => {
+export const getTodo2 = async (id) => {
   const res = await axios.get(
     `https://jsonplaceholder.typicode.com/todos/${id}`
   );
   return res.data;
 };
-
 function App() {
   console.log('render?');
-
   const id = 1;
 
-  const { data } = useQuery({
+  const { data, refetch } = useQuery({
     queryKey: ['getTodo', id],
     queryFn: () => getTodo(id),
+    placeholderData: { title: 'todo1' },
   });
 
-  // 두 번째 useQuery에서 id 변수를 사용합니다.
-  const { data: data2 } = useQuery({
+  // // 두 번째 useQuery에서 id 변수를 사용합니다.
+  const { data: data2, refetch: refetch2 } = useQuery({
     queryKey: ['getTodo2'], // 여기서는 id + 1로 예시를 들었습니다.
-    queryFn: () => getTodo222(2),
+    queryFn: () => getTodo2(2),
+    initialData: { title: 'todo2' },
   });
-
-  // const ids = [1, 2, 3]; // 예시로 1, 2, 3을 사용
-  // const results = useQueries({
-  //   queries: ids.map((id) => ({
-  //     queryKey: ['post', id],
-  //     queryFn: () => getTodo(id + 10),
-  //     staleTime: Infinity,
-  //   })),
-  // });
-
-  // useEffect(() => {
-  //   console.log('여기');
-
-  //   results.forEach((x) => {
-  //     console.log('>>>>>isLoading>>>>>>>>>>>');
-  //     console.log(x.isLoading);
-  //     console.log('>>>>>data>>>>>>>>>>>');
-  //     console.log(x.data);
-  //   });
-  // }, [results]);
+  console.log('place holder');
+  console.log(data);
+  console.log('initial data');
+  console.log(data2);
 
   return (
     <>
-      <div>test !!gd</div>
-      {/* {data && <div>Todo 1: {data.title}</div>}
-      {data2 && <div>Todo 2: {data2.title}</div>}
-      {results.map((result, index) =>
+      <div>Todo 1: {data.title}</div>
+      <div>Todo 2: {data2.title}</div>
+      <button
+        onClick={() => {
+          refetch();
+          console.log(data);
+        }}
+      >
+        여기를 클릭하면 리패치가 되어요.
+      </button>
+      <br />
+      <button
+        onClick={() => {
+          refetch2();
+          console.log(data2);
+        }}
+      >
+        여기를 클릭하면 리패치222 가 되어요.
+      </button>
+
+      {/* {data2 && <div>Todo 2: {data2.title}</div>} */}
+      {/* {results.map((result, index) =>
         result.data ? (
           <div key={index}>
             Todo {index + 1}: {result.data.title}
